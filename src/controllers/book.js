0f1644554d43b2ec8eff2e214632b06ab482a0ef
minhnhat <truongnguyenminhnhatc8@gmail.com>
2023-06-14 13:20:57 +0700

    feat: create some hppt methods

---
+++ src/controllers/book.js
@@ -0,0 +1,8 @@
+function getAllBooks(req,res){
+    res.send([
+        {name: 'Tieng chim hot', price: 20000},
+        {name: 'Chiec thuyen ngoai xaa' ,price: 15000}
+    ]);
+};
+
+module.exports ={ getAllBooks }
---
+++ src/controllers/user.js
@@ -0,0 +1,53 @@
+const user = [
+    { name: 'Nhat', age: 19 },
+    { name: 'Minh', age: 15 }
+]
+
+function getAllUser(req, res) {
+    res.send([
+        { name: 'Nhat', age: 19 },
+        { name: 'Minh', age: 15 }
+    ]);
+};
+
+//http://localhost:3000/v1/getUser/10
+function getUserById(req, res) {
+    const userId = req.params.userId;
+    console.log('userID: ', userId);
+    res.send({ userId });
+}
+
+//http://localhost:3000/v1/getUserByNameAndAge?name=nhat&age=18
+function getUserByNameAndAge(req, res) {
+    const { name, age } = req.query;
+    console.log("Name: ", name);
+    console.log("Age: ", age);
+    res.send({
+        name, age
+    })
+}
+
+
+function createUser(req, res) {
+    const { username, password, age } = req.body;
+    res.send({
+        username,
+        password,
+        age
+    })
+}
+
+const createManyUser = (req, res) => {
+    const ManyUser = req.body
+    const newUser = [...user, ...ManyUser]
+    res.send(newUser)
+
+}
+
+module.exports = {
+    getAllUser,
+    getUserById,
+    getUserByNameAndAge,
+    createUser,
+    createManyUser
+}
